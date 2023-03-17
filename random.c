9ab137c3c873d4c2f01331dc87ec9dc96b5d1f12
Namasaka <lennoxnamasaka@gmail.com>
2023-03-17 16:26:36 +0300

    running with fixed output

--- random.c
+++ random.c
@@ -9,34 +9,49 @@ int _strlen(char *str)
 		count++;
 	return (count);
 }
-char *_input(int num)
+char *_input(void)
 {
-	int i = 0;
-	char *names, *ans;
+	int i = 0, len, spc = 0, j = 0, a = 0;
+	char names[1024], *str;
 
-	printf("Please enter names! Press ENTER after each name!\n");
-	printf("Enter '\\' when done and press ENTER\n");
-	while(i < num)
+	printf("Please enter names separated by a space!\n");
+	scanf("%s\n", names);
+	while (names[i] != '\0')
 	{
-		scanf("%s\n", &names[i]);
+		if (names[i] == ' ')
+			spc++;
 		i++;
 	}
-	return (&names[1]);
+	len = i;
+	str = malloc(sizeof(char) * (len + 1));
+	for (i = 0; i <= spc; i++)
+	{
+		for (; j < len; j++)
+		{
+			if (names[j] == ' ')
+				str[i + j] = '\0';
+			else
+				str[i + j] = names[j];
+		}
+	}
+	str[len] = '\0';
+	return (&str[2]);
 }
 
 int main(void)
 {
-	int num_people, random, size = 0;
+	int random, size = 0;
 	int i;
 	char *str;
 
-	scanf("Enter number of people: %d\n", &num_people);
-	str = _input(num_people);
+	// printf("Enter number of people:\n");
+	// scanf("%d\n", &num_people);
+	str = _input();
 	if (str == NULL)
 	{
 		printf("FAILED\n");
 		return (1);
 	}
-	printf("Our Lucky Winner is: %s:\n", str);
+	printf("Our Lucky Winner is: %s\n", str);
 	return (0);
 }
