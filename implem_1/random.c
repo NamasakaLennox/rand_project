744db83d86ac24018d67f910d4eccd87d150fe3a
NamasakaLennox <namasakalennox254@gmail.com>
2023-03-19 14:54:11 +0300

    input fix --working

--- implem_1/random.c
+++ implem_1/random.c
@@ -5,19 +5,21 @@
 
 void get_random(void)
 {
-	int num_people = 0, random, i;
+	int num_people = 0, random, i, cmp;
 	char *person[30], name[50];
 
 	printf("Enter the names members, Enter 'done' when finished\n");
 	printf("Person %d: ", num_people + 1);
-	scanf("%s\n", name);
-	while (strcmp(name, "done") != 0)
+	scanf("%s", name);
+	while (1)
 	{
 		person[num_people] = malloc(sizeof(char) * (strlen(name) + 1));
 		strcpy(person[num_people], name);
+		if (strcmp(person[num_people], "done") == 0)
+			break;
 		num_people++;
 		printf("Person %d: ", num_people + 1);
-		scanf("%s\n", name);
+		scanf("%s", name);
 	}
 	if (num_people == 0)
 	{
